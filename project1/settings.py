2aba6ce8254bf1be8799643687a5c20a8c9ad4e3
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2022-08-26 11:35:03 +0800

    add middleware to get loading performance

--- project1/settings.py
+++ project1/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'project1.middlewares.StatsMiddleware',
 ]
 
 ROOT_URLCONF = 'project1.urls'
