d69820a109f8d12350c434e644ac9bf8963cbf78
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2022-09-13 09:31:23 +0800

    rekom view for admin

--- recomposition/views.py
+++ recomposition/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from multiprocessing import context
 from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -81,81 +82,138 @@ class RecompositionAKI(LoginRequiredMixin, View):
         #GET USER DIVISION, DETERMINE USER VIEW
         division = request.user.division
 
-        if division == "Super Admin" or division == "ANG": #CHANGE THIS LATER
-            lrpa = PRKData.objects.select_related('prk').filter(file_lrpa=last_lrpa)
+        if division in ["ANG", "KEU", "Super Admin"]:
+            # GET ALL VIEW
+            rekap_user = ["REN", "PPK", "OPK 1", "OPK 2", "K3L"]
+            
+            total_usulan = 0
+            missing_notes = []
+            
+            for user in rekap_user:
+                lrpa = PRKData.objects.select_related('prk').filter(file_lrpa=last_lrpa, prk__rekap_user_induk=user)
+                
+                #GET LATEST DRAFT
+                draft = UsulanRekomposisiAKI.objects.get(division=user, period=period)
+
+                #IF USULAN DATA NOT CREATED YET
+                if not draft.is_data_created:
+                    for data in lrpa:
+                        usulan_data = UsulanRekomposisiAKIData(file=draft, prk=data.prk)
+                        usulan_data.save()
+                    
+                    draft.is_data_created = True
+                    draft.save()
+                
+                #GET DATA INFO
+                for data in lrpa:
+                    usulan_data = UsulanRekomposisiAKIData.objects.get(file=draft, prk=data.prk)
+                    temp_usulan = data.get_total_realisasi()
+                    for i in range(this_month(),13):
+                        if usulan_data.get_rencana_bulan(i):
+                            temp_usulan = temp_usulan + usulan_data.get_rencana_bulan(i)
+                            if usulan_data.prk.no_prk == "2019.USLU.5.001": print(i, usulan_data.get_rencana_bulan(i))
+                        else:
+                            temp_usulan = temp_usulan + int(float(data.get_rencana_month(i) or 0))
+                            if usulan_data.prk.no_prk == "2019.USLU.5.001": print(i, int(float(data.get_rencana_month(i) or 0)))
+                    
+                    selisih_usulan = temp_usulan - data.real_aki()
+
+                    combine_list.append((data,usulan_data,temp_usulan,selisih_usulan))
+                    total_usulan = total_usulan + temp_usulan
+                    
+                    if usulan_data.is_missing_notes():
+                        msg_notes = "PRK : " + str(usulan_data.prk.no_prk) + " terdapat perubahan AKB tetapi tidak mencantumkan notes"
+                        missing_notes.append((msg_notes))
+
             context["for_div"] = "ALL"
         else:
             lrpa = PRKData.objects.select_related('prk').filter(file_lrpa=last_lrpa, prk__rekap_user_induk=division)
             context["for_div"] = division
         
-        # GET LATEST DRAFT
-        draft = UsulanRekomposisiAKI.objects.get(division=context["for_div"], period=period)
+            #GET LATEST DRAFT
+            draft = UsulanRekomposisiAKI.objects.get(division=context["for_div"], period=period)
+            context["draft_pk"] = draft.pk
 
-        # IF USULAN DATA NOT CREATED YET
-        if not draft.is_data_created:
-            for data in lrpa:
-                usulan_data = UsulanRekomposisiAKIData(file=draft, prk=data.prk)
-                usulan_data.save()
-            
-            draft.is_data_created = True
-            draft.save()
+            # CHECK IF ALREADY SUBMITTED # CHANGE TEMPLATE LATER
+            if draft.is_publish == True:
+                return render(request, '404_not_found_2.html', context)
 
-        total_usulan = 0
-        missing_notes = []
-        for data in lrpa:
-            # SELECT THROUGH THE MONTH
-            usulan_data = UsulanRekomposisiAKIData.objects.get(file=draft, prk=data.prk)
-            temp_usulan = data.get_total_realisasi()
-            for i in range(this_month(),13):
-                if usulan_data.get_rencana_bulan(i):
-                    temp_usulan = temp_usulan + usulan_data.get_rencana_bulan(i)
-                else:
-                    temp_usulan = temp_usulan + int(float(data.get_rencana_month(i) or 0))
+            # IF USULAN DATA NOT CREATED YET
             
-            selisih_usulan = temp_usulan - data.real_aki()
-
-            combine_list.append((data,usulan_data,temp_usulan,selisih_usulan))
-            total_usulan = total_usulan + temp_usulan
-            
-            if usulan_data.is_missing_notes():
-                msg_notes = "PRK : " + str(usulan_data.prk.no_prk) + " terdapat perubahan AKB tetapi tidak mencantumkan notes"
-                missing_notes.append((msg_notes))
-            
-            print(data.prk.no_prk, temp_usulan, selisih_usulan, usulan_data.is_missing_notes())
-        
-        if 'check-data' in request.GET:
-            context["show_error"] = True
-
-            total_aki = lrpa.aggregate(Sum('aki_this_year'))['aki_this_year__sum']
-            error_count = 0
-            #CHECK DEFISIT USULAN
-            if total_aki != total_usulan:
-                error_count = error_count + 1
-                msg = "Total AKI tidak sama dengan selisih usulan terdapat "
-                if total_aki < total_usulan:
-                    msg = msg + "surplus sebesar Rp."
-                    msg = msg + str(int(float(total_usulan-total_aki)))
+            if not draft.is_data_created:
+                for data in lrpa:
+                    usulan_data = UsulanRekomposisiAKIData(file=draft, prk=data.prk)
+                    usulan_data.save()
                 
-                if total_aki > total_usulan:
-                    msg = msg + "defisit sebesar Rp."
-                    msg = msg + str(int(float(total_aki-total_usulan)))
+                draft.is_data_created = True
+                draft.save()
 
-                context["aki_error"] = msg
+        
+            total_usulan = 0
+            missing_notes = []
+            for data in lrpa:
+                # SELECT THROUGH THE MONTH
+                usulan_data = UsulanRekomposisiAKIData.objects.get(file=draft, prk=data.prk)
+                temp_usulan = data.get_total_realisasi()
+                for i in range(this_month(),13):
+                    if usulan_data.get_rencana_bulan(i):
+                        temp_usulan += usulan_data.get_rencana_bulan(i)
+                    else:
+                        temp_usulan += int(float(data.get_rencana_month(i) or 0))
+                
+                selisih_usulan = temp_usulan - data.real_aki()
+
+                combine_list.append((data,usulan_data,temp_usulan,selisih_usulan))
+                total_usulan = total_usulan + temp_usulan
+                
+                if usulan_data.is_missing_notes():
+                    msg_notes = "PRK : " + str(usulan_data.prk.no_prk) + " terdapat perubahan AKB tetapi tidak mencantumkan notes"
+                    missing_notes.append((msg_notes))
             
-            #CHECK IF THERE IS MISSING NOTES
-            context["missing_notes"] = missing_notes
-            error_count = error_count + len(missing_notes)
-            context["error_count"] = error_count
-            if error_count > 0:
-                context["error_count_msg"] = "Terdapat total " + str(error_count) + " error"
-            
-            if error_count == 0:
-                context["error_count_msg"] = "Sudah tidak terdapat error, data rekomposisi sudah bisa dikirim"
+            if 'check-data' in request.GET:
+                context["show_error"] = True
+
+                total_aki = lrpa.aggregate(Sum('aki_this_year'))['aki_this_year__sum']
+                error_count = 0
+                #CHECK DEFISIT USULAN
+                if total_aki != total_usulan:
+                    error_count = error_count + 1
+                    msg = "Total AKI tidak sama dengan selisih usulan terdapat "
+                    if total_aki < total_usulan:
+                        msg = msg + "surplus sebesar Rp."
+                        msg = msg + str(int(float(total_usulan-total_aki)))
+                    
+                    if total_aki > total_usulan:
+                        msg = msg + "defisit sebesar Rp."
+                        msg = msg + str(int(float(total_aki-total_usulan)))
+
+                    context["aki_error"] = msg
+                
+                #CHECK IF THERE IS MISSING NOTES
+                context["missing_notes"] = missing_notes
+                error_count = error_count + len(missing_notes)
+                context["error_count"] = error_count
+                if error_count > 0:
+                    context["error_count_msg"] = "Terdapat total " + str(error_count) + " error"
+                
+                if error_count == 0:
+                    context["error_count_msg"] = "Sudah tidak terdapat error, data rekomposisi sudah bisa dikirim"
 
         document = [last_lrpa, last_mou]
         context["document"] = document #OPTIMIZE LATER?
 
         context["lrpa"] = combine_list
+
+        return render(request, 'recomposition/recomposition_aki.html', context)
+    
+    def post(self, request):
+        context = {}
+        draft = UsulanRekomposisiAKI.objects.get(pk=request.POST["draft-pk"])
+        draft.is_publish = True
+        draft.save()
+
+        if request.user.division not in ["ANG", "KEU", "Super Admin"] and draft.is_publish == True:
+            return render(request, '404_not_found_2.html', context)
 
         return render(request, 'recomposition/recomposition_aki.html', context)
 
@@ -195,7 +253,6 @@ class UsulanRekomposisiEdit(LoginRequiredMixin, View):
             prk_draft = UsulanRekomposisiAKIData.objects.get(file=draft, prk=prk)
             value_1 = prk_draft.get_rencana_bulan(month) #MANUAL
             notes = prk_draft.notes
-
         except:
             value_1 = 0
             notes = ""
@@ -203,20 +260,21 @@ class UsulanRekomposisiEdit(LoginRequiredMixin, View):
         if value_1:
             context["value_draft"] = str(int(float(value_1)))
         else:
-            context["value_draft"] = str(int(float(value)))
+            context["value_draft"] = str(int(float(value or 0)))
 
         context["this_month"] = month
         context["month"] = months[month]
         context["selisih"] = str(value) #MANUAL
         context["notes"] = notes
-        context["is_month"] = prk_draft.is_rencana_bulan(month)
-        print(context["is_month"])
+        
+        try:
+            context["is_month"] = prk_draft.is_rencana_bulan(month)
+        except:
+            context["is_month"] = 0
 
         return render(request, 'recomposition/snippets/modal_edit.html', context)
 
     def post(self, request, pk, type, *args, **kwargs):
-        context = {}
-
         pass
 
 class OnChangeValue(LoginRequiredMixin, View):
